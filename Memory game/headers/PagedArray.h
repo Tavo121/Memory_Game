2b862b5111cb6dc8825575c9fdbbe85284ce8eab
Tavo <galvarado13@estudiantec.cr>
2022-04-22 17:22:43 -0600

    limpieza de codigo

--- Memory game/headers/PagedArray.h
+++ Memory game/headers/PagedArray.h
@@ -35,7 +35,7 @@ public:
     ServerInterface* interfaceInstance;
 
 private:
-    int size, iCopy, jCopy;
+    int size;
     vector<Card*> memoryCards;
     void allocateCards(), saveCardToDisk(), loadCard(int i, int j);
     bool searchCardInMemory(string pos);
